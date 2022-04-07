5fb85f93b97455e8a23dafa0cc5ad37e35c98c2a
ThePhenom-Pro <thephenompro@gmail.com>
2022-04-07 21:28:22 +0300

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /** 
  * main - uses puts() to output a string
  * Return: 0 if the output is successful
  */
-
 int main(void)
 {
    puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-
 /**
  * main - uses printf to output a string
- * 
  * Return: 0 if there is no error
  */
-
 int main(void)
 {
    printf("with proper grammar, but the outcome is a piece of art,\n");
