5dd3b92b8054b5df491a63e0c4b787dcac918375
ThePhenom-Pro <thephenompro@gmail.com>
2022-04-09 02:37:31 +0300

    check

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,12 +8,7 @@ int main(void)
 {
 int c;
 int d;
-int e;
-int f = 0;
-
-while (f < 10)
-{
-e = 0;
+int e = 0;
 while (e < 10)
 {
 d = 0;
@@ -22,26 +17,24 @@ while (d < 10)
 c = 0;
 while (c < 10)
 {
-if (!(f == c && e == d))
+if (c != d && d != e && e < d && d < c)
 {
-putchar('0' + f);
 putchar('0' + e);
-putchar(' ');
 putchar('0' + d);
 putchar('0' + c);
-if (!(f + e == 18 && c + d == 17 && d == 9))
+
+if (c + d + e != 9 + 8 + 7)
 {
 putchar(',');
 putchar(' ');
 }
 }
+
 c++;
 }
 d++;
 }
 e++;
-}
-f++;
 }
 putchar('\n');
 return (0);
