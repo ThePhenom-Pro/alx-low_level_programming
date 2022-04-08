3cb39a6a75108b581c3ae45a9c96255c27886eca
ThePhenom-Pro <thephenompro@gmail.com>
2022-04-09 00:43:57 +0300

    betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,19 +2,18 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - determines the parity of a random number
- * 
- * Return: 0 if it is successful 
- */
+* main - determines the parity of a random number
+* Return: 0 if it is successful
+*/
 int main(void)
 {
 int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n>0)
+if (n > 0)
     printf("%d is positive\n", n);
-else if(n==0)
+else if(n == 0)
     printf("%d is zero\n", n);
 else
     printf("%d is negative\n", n);
