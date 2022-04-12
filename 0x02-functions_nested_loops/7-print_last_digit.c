e822c22897ccd6c60ec83c06dc2661bf28243359
ThePhenom-Pro <thephenompro@gmail.com>
2022-04-12 22:20:21 +0300

    trial

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,7 +6,11 @@
  */
 int print_last_digit(int n)
 {
-int last_digit = ((n < 0 ? -1 : 1) * n) % 10;
+int last_digit;
+if (last_digit < 0)
+last_digit = -1 * (n % 10);
+else
+last_digit = n % 10;
 _putchar('0' + last_digit);
 return (last_digit);
 }
