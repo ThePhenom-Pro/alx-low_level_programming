7ae42014d612239fd588119474e83fa64322a984
ThePhenom-Pro <thephenompro@gmail.com>
2022-04-12 22:47:12 +0300

    check

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
- * times_table - prints times table
+ * print_times_table - prints times table
  * @n : times table to use
  * Return:void
  */
+
 void print_times_table(int n)
 {
 int a = 0, rep, b;
